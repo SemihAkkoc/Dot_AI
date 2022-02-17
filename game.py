614bf91c7744e2c7348eab7bdbda271f9d65a1ee
Semih Akkoç <52207216+SemihAkkoc@users.noreply.github.com>
2022-02-17 20:54:19 +0300

    Update game.py

--- game.py
+++ game.py
@@ -32,20 +32,22 @@ root.update()
 total_reached_goal = []
 
 
-while True:
-    if not population.is_all_dots_dead():
-        population.update()
-        root.update()
-        sleep(0.01)
+try:
+    while True:
+        if not population.is_all_dots_dead():
+            population.update()
+            root.update()
+            sleep(0.01)
 
-    else:
-        main_window.delete('all')
-        goal = main_window.create_oval(Dot.goal_position[0], Dot.goal_position[1], Dot.goal_position[0]+Dot.goal_position[2], Dot.goal_position[1]+Dot.goal_position[2], fill='blue')
-        obsticals = Obstical(main_window, state=STATE)  # 4th state
-        population.calc_fitnesses()
-        total_reached_goal.append(population.how_many())
-        population.natural_selection()
-        population.mutate_population()
-        curr_gen_label.config(text=f'Generation: {Population.gen} |  Dots Reached Goal:  {total_reached_goal[Population.gen-2]} / 1000')
-        plot_progress(total_reached_goal)
-
+        else:
+            main_window.delete('all')
+            goal = main_window.create_oval(Dot.goal_position[0], Dot.goal_position[1], Dot.goal_position[0]+Dot.goal_position[2], Dot.goal_position[1]+Dot.goal_position[2], fill='blue')
+            obsticals = Obstical(main_window, state=STATE)  # 4th state
+            population.calc_fitnesses()
+            total_reached_goal.append(population.how_many())
+            population.natural_selection()
+            population.mutate_population()
+            curr_gen_label.config(text=f'Generation: {Population.gen} |  Dots Reached Goal:  {total_reached_goal[Population.gen-2]} / 1000')
+            plot_progress(total_reached_goal)
+except:
+    pass
